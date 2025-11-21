9b2ead424804be03bae56c416dee3036102cb1b1
hanny <grasc.hanny@gmail.com>
2025-11-21 11:17:21 +0700

    mengganti file index

--- index.php
+++ index.php
@@ -1,12 +1,13 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="id">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="style.css">
+    <title>Hello Coxo</title>
+    <link rel="stylesheet" href="style.css"> 
 </head>
 <body>
-    <p>Ya</p>
+    <?php
+    echo "Hello, Mochi!";
+    ?>
 </body>
 </html>
